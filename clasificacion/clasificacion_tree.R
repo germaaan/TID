7ead3301da2acc36d7cd6a4d2ff77ee50dd926c7
German Martinez <germaaan@gmail.com>
2016-01-12 18:21:33 +0100

    Prueba de clasificación

--- clasificacion/clasificacion_tree.R
+++ clasificacion/clasificacion_tree.R
@@ -1,27 +1,40 @@
 install.packages("tree")
 library("tree")
 
-titanic=read.csv(file="/home/germaaan/proyectos/TID/titanic.csv", header=TRUE, sep=",", dec=",")
+test=read.csv(file="/home/germaaan/proyectos/TID/test.csv", header=TRUE, sep=",", dec=",")
 
-titanic2=titanic
-titanic2$Superviviente=as.factor(ifelse(titanic2$Superviviente==1, "Superviviente", "Fallecido"))
+test2=test
+#test2$UsoTV=as.factor(ifelse(test2$UsoTV==1, "Ve TV", "No ve TV"))
+#test2$UsoRRSS=as.factor(ifelse(test2$UsoRRSS==1, "Usa RRSS", "No usa RRSS"))
+test2$Sexo=ifelse(test2$Sexo == "Hombre", 1, 0)
+test2$Soltero=ifelse(test2$EstadoCivil == "Soltero", 1, 0)
+test2$Casado=ifelse(test2$EstadoCivil == "Casado", 1, 0)
+test2$Otro=ifelse(test2$EstadoCivil == "Otro", 1, 0)
+test2$Primarios=ifelse(test2$Estudios == "Primarios", 1, 0)
+test2$Secundarios=ifelse(test2$Estudios == "Secundarios", 1, 0)
+test2$Universitarios=ifelse(test2$Estudios == "Universitarios", 1, 0)
+test2$EstadoCivil=NULL
+test2$Estudios=NULL
 
 # Crear conjuntos aleatorios de entrenamiento y prueba (70% / 30%)
-id=sample(2, nrow(titanic), replace=TRUE, prob=c(0.7, 0.3))
-entrenamiento=titanic2[id==1, ]
-prueba=titanic2[id==2, ]
+id=sample(2, nrow(test), replace=TRUE, prob=c(0.7, 0.3))
+entrenamiento=test2[id==1, ]
+prueba=test2[id==2, ]
 
 # Definir modelo para la predicción
-modelo=Superviviente~Clase+Edad+HermanosConyuges+PadresHijos+Tarifa
+modelo=UsoTV~.
+summary(lm(modelo,data=entrenamiento))
 # Crear árbol
+modelo=UsoTV~Sexo+PerCasa+NumTV+TVPer+Secundarios
 arbol=tree(modelo, data=entrenamiento)
-table(predict(arbol, type="class"), entrenamiento$Superviviente)
+table(predict(arbol, entrenamiento), entrenamiento$UsoTV)
 summary(arbol)
 plot(arbol) ; text(arbol)
+mean(predict$class==entrenamiento$UsoTV)
 
 # Test de los resultados
 prediccion=predict(arbol, newdata=prueba, type="class")
-test=table(prediccion, prueba$Superviviente)
+test=table(prediccion, prueba$UsoTV)
 
 diagonal=diag(test)
 bien_clasificados_tree=(sum(diagonal)/nrow(prueba))*100
